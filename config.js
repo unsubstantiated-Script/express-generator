4d7e71c2ae46c959e78bb84df186203fea9a0e11
Justin Fulton <jusfulton@gmail.com>
2020-08-24 21:18:21 -0700

    oath for FB added

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '644568666200420',
+        clientSecret: 'fcbfa5b8a079a9f155171e5350d05145'
+    }
 }
